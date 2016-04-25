3348fbea5e69c32bcd5acf7d0addfdb50a035641
Marcos <marcosvgf95@gmail.com>
2016-04-25 00:12:28 -0300

    24/04/16

--- index.php
+++ index.php
@@ -13,6 +13,51 @@ header("Content-Type: text/html; charset=ISO-8859-1", true);
  * Time: 17:31
  */
 
+function frequency ($file){
+	$textos = file_get_contents($file);
+    $novo = fopen("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt","a+");
+    $final = fopen("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaF01.txt","a+");
+    $k=0;
+    $texto = "";
+    $palavras = array();
+    $words = explode("|",$textos);
+    for($i=0 ; $i < sizeof($words); $i++){
+    	$parada = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/stopwords.txt");
+    	$paradas = explode("|",$parada);
+    	$l = 0;
+    	//echo "palavra: ".$words[$i]."<br>";
+    	for($j=0 ; $j < sizeof($paradas) ; $j++){
+         	if ($words[$i] == $paradas[$j]){
+         		$l = 1;
+           		break;
+         	}
+         }
+         if($l == 0 ){
+          	$texto = $texto.$words[$i]."|";
+          	$escrita = $words[$i]."|";
+          	$escreve = fwrite($novo,$escrita);
+         }
+
+    }
+    fclose($novo);
+    $new = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt");
+    $ne = explode("|",$new);
+    for ($i=0 ; $i<sizeof($ne); $i++){
+     	//echo $ne[$i];
+       	$qtd = substr_count($texto,$ne[$i]);
+      	$palavra[$ne[$i]] = $qtd;
+    }
+    arsort($palavra);
+    foreach($palavra as $chave => $valor) {
+    	if($chave != "\r\n"){
+         	$escreve = fwrite($final,$chave."-".$valor."\n");
+        }
+   	}
+    fclose($final);
+    break;
+	unlink("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt");
+}
+
 ///GERA FREQUENCIA DE PALAVRAS DO TRANSCRITO ORIGINAL
     	$textos = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aula01.txt");
         $novo = fopen("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt","a+");
@@ -57,49 +102,7 @@ header("Content-Type: text/html; charset=ISO-8859-1", true);
     	break;
 		unlink("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt");
 
-///GERA FREQUENCIA DE PALAVRAS COM TRANSCRITO DO SOFTWARE CORUJA
-		$textos = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aula01.txt");
-        $novo = fopen("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt","a+");
-        $final = fopen("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaF01.txt","a+");
-        $k=0;
-        $texto = "";
-        $palavras = array();
-        $words = explode("|",$textos);
-        for($i=0 ; $i < sizeof($words); $i++){
-            $parada = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/stopwords.txt");
-            $paradas = explode("|",$parada);
-            $l = 0;
-            //echo "palavra: ".$words[$i]."<br>";
-            for($j=0 ; $j < sizeof($paradas) ; $j++){
-                if ($words[$i] == $paradas[$j]){
-                    $l = 1;
-                    break;
-                }
-            }
-            if($l == 0 ){
-                $texto = $texto.$words[$i]."|";
-                $escrita = $words[$i]."|";
-                $escreve = fwrite($novo,$escrita);
-            }
 
-        }
-        fclose($novo);
-        $new = file_get_contents("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt");
-        $ne = explode("|",$new);
-        for ($i=0 ; $i<sizeof($ne); $i++){
-            //echo $ne[$i];
-            $qtd = substr_count($texto,$ne[$i]);
-            $palavra[$ne[$i]] = $qtd;
-        }
-        arsort($palavra);
-        foreach($palavra as $chave => $valor) {
-            if($chave != "\r\n"){
-                $escreve = fwrite($final,$chave."-".$valor."\n");
-            }
-		}
-        fclose($final);
-    	break;
-		unlink("/home/marcos/Área de Trabalho/Transcritos/TranscritosOriginais/aulaP01.txt");
 
 ///ENVIA ARQUIVO PARA SOFTWARE AUDIMUS E AGUARDA RETORNO DE TRANSCRIÇÃO
 /*
@@ -122,6 +125,7 @@ while($return == 1){
     echo "return : $return";
 }
 */
+
 
 ?>
 </body>
